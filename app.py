1f1a1c705f07d5281f0975dd50488134dbcd0950
Adam Denes <adam.denes@icloud.com>
2020-03-14 14:44:03 +0100

    Pushing initial files to repository

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+Python Web Development Techdegree
+Project 2 - Basketball Stats Tool
+--------------------------------
+"""
+
+from stats import constants  # basketball-team-stats_v1
+
+
+def run_app():
+    pass
+
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    run_app()
